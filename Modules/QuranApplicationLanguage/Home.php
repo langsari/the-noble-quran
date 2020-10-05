bd107403577c5bb28bf03114b64673a73ea0e575
Mary Mionic <watchdog.av@gmail.com>
2020-10-05 09:38:19 +0700

    1

--- Modules/QuranApplicationLanguage/Home.php
+++ Modules/QuranApplicationLanguage/Home.php
@@ -1,9 +1,23 @@
-<?php
-require 'vendor/autoload.php';
-use Symfony\Component\Translation\Translator;
-use Symfony\Component\Translation\MessageSelector;
-?>
-
-<?php
-$translator = new Translator('fr_FR', new MessageSelector());
-?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+</head>
+<body>
+    <?php
+    use Symfony\Component\Translation\Translator;
+    use Symfony\Component\Translation\MessageSelector;
+    ?>
+    <?php
+    $translator = new Translator('fr_FR', new MessageSelector());
+    ?>
+    <?php
+    $translator->addResource('array', array(
+        'Hello World!' => 'Bonjour',
+    ), 'fr_FR');
+    ?>
+    <?php
+    echo "Hello World!";
+    ?>
+</body>
+</html>
