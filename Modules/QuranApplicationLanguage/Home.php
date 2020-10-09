3c29f5be3339fcd3ac0b0209d6d0f1f110c7ba57
Mary Mionic <watchdog.av@gmail.com>
2020-10-09 20:37:42 +0700

    Dlt

--- Modules/QuranApplicationLanguage/Home.php
+++ Modules/QuranApplicationLanguage/Home.php
@@ -4,12 +4,6 @@
 <title>Quran.com</title>
 </head>
 <body>
-    <?php
-    include ('M:\University Works\Modern Language\the-noble-quran\vendor\autoload.php');
-
-    $client = new \Goutte\Client();
-    $scrapping = $client->request('GET', 'https://quran.com/?local=en');
-    $fullhtmlpage = $scrapping->html();
-    ?>
+    
 </body>
 </html>
