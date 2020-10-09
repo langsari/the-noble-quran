f81974a67fca45738217004976b01eaa740c6af3
Mary Mionic <watchdog.av@gmail.com>
2020-10-09 21:04:42 +0700

    ed

--- Modules/QuranApplicationLanguage/Home.php
+++ Modules/QuranApplicationLanguage/Home.php
@@ -16,10 +16,10 @@
             $quran_doc->loadHTML($html);
             libxml_clear_errors(); //remove errors for yucky html
             
-            $pokemon_xpath = new DOMXPath($quran_doc);
+            $quran_xpath = new DOMXPath($quran_doc);
 
             //get all the h2's with an id
-            $quran_row = $pokemon_xpath->query('//div[@class]');
+            $quran_row = $quran_xpath->query('//div[@class]');
 
             if($quran_row->length > 0){
                 foreach($quran_row as $row){
