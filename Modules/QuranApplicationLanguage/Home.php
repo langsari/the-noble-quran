8e65fb1714fca888be9a8a7ecaa617bf88480538
Mary Mionic <watchdog.av@gmail.com>
2020-10-09 21:03:31 +0700

    Ed

--- Modules/QuranApplicationLanguage/Home.php
+++ Modules/QuranApplicationLanguage/Home.php
@@ -7,22 +7,22 @@
     <?php
         $html = file_get_contents('https://quran.com/?local=en'); //get the html returned from the following url
 
-        $pokemon_doc = new DOMDocument();
+        $quran_doc = new DOMDocument();
 
         libxml_use_internal_errors(TRUE); //disable libxml errors
 
         if(!empty($html)){ //if any html is actually returned
 
-            $pokemon_doc->loadHTML($html);
+            $quran_doc->loadHTML($html);
             libxml_clear_errors(); //remove errors for yucky html
             
-            $pokemon_xpath = new DOMXPath($pokemon_doc);
+            $pokemon_xpath = new DOMXPath($quran_doc);
 
             //get all the h2's with an id
-            $pokemon_row = $pokemon_xpath->query('//div[@class]');
+            $quran_row = $pokemon_xpath->query('//div[@class]');
 
-            if($pokemon_row->length > 0){
-                foreach($pokemon_row as $row){
+            if($quran_row->length > 0){
+                foreach($quran_row as $row){
                     echo $row->nodeValue . "<br/>";
                 }
             }
