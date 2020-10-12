fa78b3c451cb58c5a864214a58d5d8455a00e9a5
Mary Mionic <watchdog.av@gmail.com>
2020-10-12 14:39:02 +0700

    scrp#2

--- Modules/QuranApplicationLanguage/Home.php
+++ Modules/QuranApplicationLanguage/Home.php
@@ -7,17 +7,17 @@
     <?php
     //Here is webscraping code section 
         include('M:\University Works\Modern Language\the-noble-quran\src\simple_html_dom.php');
-
+        //Requests via url
         $quranPage = file_get_html('https://quran.com/?local=en');
-        echo $quranPage->find('title',0)->plaintext;
-
+        
+        //Navbar Scrp
         $nav = $quranPage->find('div[class="container-fluid"]',0);
-        $nav_array = $nav->find('ul');
+        $nav_array = $nav->find('ul[class="hidden-xs hidden-sm nav navbar-nav navbar-right"]');
         //Iterate over navbar for finding the needed elements inside nav
         for($i = 0; $i < sizeof($nav_array); $i++){
             echo $nav_array[$i];
-        }
-
+            echo "<br>";
+        };
     ?>
 </body>
 </html>
