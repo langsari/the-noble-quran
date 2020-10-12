c26b84e6897ad4f58152b2995dbe8af3bcf094b3
Mary Mionic <watchdog.av@gmail.com>
2020-10-12 14:47:46 +0700

    #4

--- Modules/QuranApplicationLanguage/Home.php
+++ Modules/QuranApplicationLanguage/Home.php
@@ -2,6 +2,7 @@
 <html>
 <head>
 <title>Quran.com</title>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 </head>
 <body>
     <?php
@@ -18,6 +19,11 @@
             echo $nav_array[$i];
             echo "<br>";
         };
+
     ?>
+
+    <div class='container-fliud .bg-dark'>
+        
+    </div>
 </body>
 </html>
