efdcacfa1b8d505de0e14bbd8cecad94ba674606
Mary Mionic <watchdog.av@gmail.com>
2020-10-13 10:30:01 +0700

    add Homepage.php

--- Modules/QuranApplicationLanguage/Home.php
+++ Modules/QuranApplicationLanguage/Home.php
@@ -5,25 +5,31 @@
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 </head>
 <body>
-    <?php
-    //Here is webscraping code section 
-        include('M:\University Works\Modern Language\the-noble-quran\src\simple_html_dom.php');
-        //Requests via url
-        $quranPage = file_get_html('https://quran.com/?local=en');
-        
-        //Navbar Scrp
-        $nav = $quranPage->find('div[class="container-fluid"]',0);
-        $nav_array = $nav->find('ul[class="hidden-xs hidden-sm nav navbar-nav navbar-right"]');
-        //Iterate over navbar for finding the needed elements inside nav
-        for($i = 0; $i < sizeof($nav_array); $i++){
-            echo $nav_array[$i];
-            echo "<br>";
-        };
+    <div class='container-fliud bg-dark'>
+        <div style='color:red'>
+            <?php
+                //import simple_html_dom_parser
+                include('M:\University Works\Modern Language\the-noble-quran\src\simple_html_dom.php');
 
-    ?>
+                //Requests via url
+                $quranPage = file_get_html('https://quran.com/?local=en');
 
-    <div class='container-fliud .bg-dark'>
-        
+                $hello = 'hello';
+                
+                //Navbar Scrp
+                $nav = $quranPage->find('div[class="container-fluid"]',0);
+                $nav_array = $nav->find('ul[class="hidden-xs hidden-sm nav navbar-nav navbar-right"]');
+                //Iterate over navbar for finding the needed elements inside nav
+                for($i = 0; $i < sizeof($nav_array); $i++){
+                    echo $nav_array[$i];
+                    echo $hello;
+                    echo "<br>";
+                };
+            ?>
+        </div>
     </div>
+    <?php
+        $quranBody = $quranPage->find('div[class="_3L7utArEPWqCQEMpZxj92J"]')->find('div')
+    ?>
 </body>
 </html>
