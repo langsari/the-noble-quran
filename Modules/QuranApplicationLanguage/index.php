1668644e1138735d3e4f86f7d508e623488f7a1a
Mary Mionic <watchdog.av@gmail.com>
2020-10-19 12:21:00 +0700

    before move navbar

--- Modules/QuranApplicationLanguage/index.php
+++ Modules/QuranApplicationLanguage/index.php
@@ -1,13 +1,13 @@
 <?php
 include 'Template.php';
+include 'LangsCatalog.php';
  $template = new Template;
 
- $alhadit = array('Al-Hadith', 'ฮาดิซ', 'Hadis');
 
+ $alhadit = array('Al-Hadith', 'ฮาดิซ', 'Hadis');
  $template->assign('navbarBrand', 'Quran.com');
  $template->assign('Al-Hadith', $alhadit[1]);
  $template->assign('Al-Quran', 'อัลกุรอ่าน');
-
-
+ $template->assign('loop', $c);
  $template->render('index');
 ?>
