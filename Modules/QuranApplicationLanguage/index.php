5ee7bed7d0fecdfeef19d5c46c212deec67f293e
Mary Mionic <watchdog.av@gmail.com>
2020-10-26 08:00:37 +0700

    Use Twig

---
+++ Modules/QuranApplicationLanguage/index.php
@@ -0,0 +1,11 @@
+<?php
+require_once("../../vendor/autoload.php");
+
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+
+$loader = new FilesystemLoader(__DIR__. '/template');
+$twig = new Environment($loader); 
+
+echo $twig->render("index.html.twig")
+?>
