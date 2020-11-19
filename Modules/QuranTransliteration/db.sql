af80ea65a650f719fee6a5e1ed4b8bfee4a36c9f
Ameen25018 <59605463+Ameen25018@users.noreply.github.com>
2020-11-19 21:47:06 +0700

    Add files via upload

---
+++ Modules/QuranTransliteration/db.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE transliterataion;
+CREATE TABLE `eng` (
+ 
+  `Transliterataion` varchar(100) NOT NULL,
+  `Ayat` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+//import file csv in PHPMYADMIN
