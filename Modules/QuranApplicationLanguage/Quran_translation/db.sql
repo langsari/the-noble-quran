9a2e25c6bfc97d14c04ccfa31bf646841744d71b
Ameen25018 <59605463+Ameen25018@users.noreply.github.com>
2020-11-19 21:43:16 +0700

    Delete db.sql

--- Modules/QuranApplicationLanguage/Quran_translation/db.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE transliterataion;
-CREATE TABLE `eng` (
- 
-  `Transliterataion` varchar(100) NOT NULL,
-  `Ayat` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
-//import file csv in PHPMYADMIN
