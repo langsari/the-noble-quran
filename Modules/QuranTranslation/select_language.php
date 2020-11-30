74e8eb3e29c1de419e9f305fa901aa06d34bf6c3
iaffa <60456270+iaffa@users.noreply.github.com>
2020-12-01 03:12:24 +0700

    Update select_language.php

--- Modules/QuranTranslation/select_language.php
+++ Modules/QuranTranslation/select_language.php
@@ -11,14 +11,29 @@
  
   <select class="form-control" name="id" id="id">
     <option value="" disabled selected>Select Language</option>
+    <option value="albanian.php">Albanian</option> 
+    <option value="amazigh.php">Amazigh</option>
+    <option value="amharic.php">Amharic</option>
+    <option value="bengali.php">Bengali</option>
+    <option value="bosnian.php">Bosnian</option>
+    <option value="bulgarian.php">Bulgarian</option>
     <option value="chinese.php">Chinese</option>
+    <option value="czech.php">Czech</option>
+    <option value="divehi.php">Divehi</option>
+    <option value="dutch.php">Dutch</option>
     <option value="english.php">English</option>
+    <option value="french.php">French</option>
     <option value="german.php">German</option>
+    <option value="hausa.php">Hausa</option>
+    <option value="hindi.php">Hindi</option>
     <option value="indonesian.php">Indonesian</option>
     <option value="italian.php">Italian</option>
     <option value="japanese.php">Japanese</option>
     <option value="korean.php">Korean</option>
+    <option value="kurdish.php">Kurdish</option>
+    <option value="malayalam.php">Malayalam</option>
     <option value="malay.php">Malay</option>
+    <option value="norwegian.php">Norwegian</option>
     <option value="portuguese.php">Portuguese</option>
     <option value="russian.php">Russian</option>
     <option value="spanish.php">Spanish</option>
@@ -26,6 +41,16 @@
     <option value="thai.php">ภาษาไทย</option>
     <option value="turkish.php">Turkish</option>
     <option value="urdu.php">Urdu</option>
+
+    
+   
+ 
+    
+    
+    
+   
+  
+    
   </select>
 
 </h2>
