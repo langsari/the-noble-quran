24318dd45b6fc2b29b9d823b3665d9e736a9f709
iaffa <60456270+iaffa@users.noreply.github.com>
2020-12-01 03:14:03 +0700

    update translate

---
+++ Modules/QuranTranslation/malayalam.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <style>
+    table {
+      font-family: arial, sans-serif;
+      border-collapse: collapse;
+      width: 100%;
+    }
+
+    td,
+    th {
+      border: 1px solid #dddddd;
+      text-align: left;
+      padding: 8px;
+    }
+
+    tr:nth-child(even) {
+      background-color: #dddddd;
+    }
+  </style>
+</head>
+
+<body>
+
+  <?php
+  include('db.php');
+  include('select_language.php');
+  include('select_surah.php');
+  ?>
+
+
+  <table>
+    <thead>
+      <tr>
+        <th width="1">No.</th>
+        <th width="1">Surah:Ayat</th>
+        <th width="1">Quran</th>
+        <th width="1">Malayalam</th>
+      </tr>
+      <thead>
+
+      <tbody>
+
+        <?php
+
+
+if (extract($_GET)) {
+  $surah = isset($_GET['surah']) ? $_GET['surah'] : "";
+  if (is_numeric($surah)) {
+  $sql = "SELECT Arabic.*, Malayalam.* FROM Arabic,Malayalam WHERE Arabic.index_arabic=Malayalam.index_malayalam AND sura_arabic=$surah ORDER BY index_arabic ASC";
+  } else if ($surah == "all") {
+    $sql = "SELECT Arabic.*, Malayalam.* FROM Arabic,Malayalam WHERE Arabic.index_arabic=Malayalam.index_malayalam ORDER BY index_arabic ASC";
+  }
+}
+        $result = mysqli_query($connect, $sql);
+        while ($row = mysqli_fetch_array($result)) {
+
+          $index_arabic = $row["index_arabic"];
+          $sura_arabic = $row["sura_arabic"];
+          $aya_arabic = $row["aya_arabic"];
+          $text_arabic = $row["text_arabic"];
+
+          $index_malayalam = $row["index_malayalam"];
+          $sura_malayalam = $row["sura_malayalam"];
+          $aya_malayalam = $row["aya_malayalam"];
+          $text_malayalam = $row["text_malayalam"];
+
+        ?>
+
+          <tr>
+
+            <td><?php echo $index_arabic ?></td>
+            <td><?php echo $sura_arabic . ":" . $aya_arabic ?></td>
+            <td><?php echo "<div align='right'>" . $text_arabic . "</div>";?></td>
+            <td><?php echo $text_malayalam ?></td>
+          </tr>
+        <?php
+        }
+
+        ?>
+
+      </tbody>
+
+  </table>
+
+</body>
+
+</html>
