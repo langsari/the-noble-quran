6f5c971c6d7115641dab9523184937114502f210
iaffa <60456270+iaffa@users.noreply.github.com>
2020-12-04 00:56:12 +0700

    update eng

---
+++ Modules/QuranTranslation/en_pickthall.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <style>
+    table {
+      font-family: arial, sans-serif;
+      border-collapse: collapse;
+      width: 100%;
+    }
+
+    td,
+    th {
+      border: 1px solid #dddddd;
+      text-align: left;
+      padding: 8px;
+    }
+
+    tr:nth-child(even) {
+      background-color: #dddddd;
+    }
+  </style>
+</head>
+
+<body>
+
+  <?php
+  include('db.php');
+  include('select_language.php');
+  include('select_surah.php');
+  ?>
+
+
+  <table>
+    <thead>
+      <tr>
+        <th width="1">No.</th>
+        <th width="1">Surah:Ayat</th>
+        <th width="1">Quran</th>
+        <th width="1">English : Pickthall </th>
+      </tr>
+      <thead>
+
+      <tbody>
+
+        <?php
+
+
+if (extract($_GET)) {
+  $surah = isset($_GET['surah']) ? $_GET['surah'] : "";
+  if (is_numeric($surah)) {
+  $sql = "SELECT Arabic.*, En_pickthall.* FROM Arabic,En_pickthall WHERE Arabic.index_arabic=En_pickthall.index_en_pickthall AND sura_arabic=$surah ORDER BY index_arabic ASC";
+  } else if ($surah == "all") {
+    $sql = "SELECT Arabic.*, En_pickthall.* FROM Arabic,En_pickthall WHERE Arabic.index_arabic=En_pickthall.index_en_pickthall ORDER BY index_arabic ASC";
+  }
+}
+        $result = mysqli_query($connect, $sql);
+        while ($row = mysqli_fetch_array($result)) {
+
+          $index_arabic = $row["index_arabic"];
+          $sura_arabic = $row["sura_arabic"];
+          $aya_arabic = $row["aya_arabic"];
+          $text_arabic = $row["text_arabic"];
+
+          $index_en_pickthall = $row["index_en_pickthall"];
+          $sura_en_pickthall = $row["sura_en_pickthall"];
+          $aya_en_pickthall = $row["aya_en_pickthall"];
+          $text_en_pickthall = $row["text_en_pickthall"];
+
+        ?>
+
+          <tr>
+
+            <td><?php echo $index_arabic ?></td>
+            <td><?php echo $sura_arabic . ":" . $aya_arabic ?></td>
+            <td><?php echo "<div align='right'>" . $text_arabic . "</div>";?></td>
+            <td><?php echo $text_en_pickthall ?></td>
+          </tr>
+        <?php
+        }
+
+        ?>
+
+      </tbody>
+
+  </table>
+
+</body>
+
+</html>
---
+++ Modules/QuranTranslation/en_sahih.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <style>
+    table {
+      font-family: arial, sans-serif;
+      border-collapse: collapse;
+      width: 100%;
+    }
+
+    td,
+    th {
+      border: 1px solid #dddddd;
+      text-align: left;
+      padding: 8px;
+    }
+
+    tr:nth-child(even) {
+      background-color: #dddddd;
+    }
+  </style>
+</head>
+
+<body>
+
+  <?php
+  include('db.php');
+  include('select_language.php');
+  include('select_surah.php');
+  ?>
+
+
+  <table>
+    <thead>
+      <tr>
+        <th width="1">No.</th>
+        <th width="1">Surah:Ayat</th>
+        <th width="1">Quran</th>
+        <th width="1">English : Saheeh International</th>
+      </tr>
+      <thead>
+
+      <tbody>
+
+        <?php
+
+
+if (extract($_GET)) {
+  $surah = isset($_GET['surah']) ? $_GET['surah'] : "";
+  if (is_numeric($surah)) {
+  $sql = "SELECT Arabic.*, En_sahih.* FROM Arabic,En_sahih WHERE Arabic.index_arabic=En_sahih.index_en_sahih AND sura_arabic=$surah ORDER BY index_arabic ASC";
+  } else if ($surah == "all") {
+    $sql = "SELECT Arabic.*, En_sahih.* FROM Arabic,En_sahih WHERE Arabic.index_arabic=En_sahih.index_en_sahih ORDER BY index_arabic ASC";
+  }
+}
+        $result = mysqli_query($connect, $sql);
+        while ($row = mysqli_fetch_array($result)) {
+
+          $index_arabic = $row["index_arabic"];
+          $sura_arabic = $row["sura_arabic"];
+          $aya_arabic = $row["aya_arabic"];
+          $text_arabic = $row["text_arabic"];
+
+          $index_en_sahih = $row["index_en_sahih"];
+          $sura_en_sahih = $row["sura_en_sahih"];
+          $aya_en_sahih = $row["aya_en_sahih"];
+          $text_en_sahih = $row["text_en_sahih"];
+
+        ?>
+
+          <tr>
+
+            <td><?php echo $index_arabic ?></td>
+            <td><?php echo $sura_arabic . ":" . $aya_arabic ?></td>
+            <td><?php echo "<div align='right'>" . $text_arabic . "</div>";?></td>
+            <td><?php echo $text_en_sahih ?></td>
+          </tr>
+        <?php
+        }
+
+        ?>
+
+      </tbody>
+
+  </table>
+
+</body>
+
+</html>
