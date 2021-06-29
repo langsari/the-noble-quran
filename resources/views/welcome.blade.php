ab4f24e3f15de5708bfcf3ed43d4f7e9c9db3226
langsari <72065442+HasanXfatanE@users.noreply.github.com>
2021-06-29 17:27:40 +0700

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Hasan
+                    The-nobal-quran
                 </div>
 
                 <div class="links">
