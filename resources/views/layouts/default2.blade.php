e9e68125fc11e3bdc7167d3aeaa3766abdce3db8
HasanXfatanE <72065442+HasanXfatanE@users.noreply.github.com>
2021-08-17 22:40:11 +0700

    Add files via upload

---
+++ resources/views/layouts/default2.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+
+    <header>
+        <h1 class="font-bold"> Header</h1>
+    </header>
+
+    <main>
+        @yield('page-content')
+    </main>
+
+    <footer>Footer</footer>
+    
+</body>
+</html>
---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,47 @@
+  <div class="footer">
+        <div class="container">
+            <div class="content-page">
+                <div class="footer-links">
+                    <div>
+                        <h5><span class='en '>Navigate</span></h5>
+                        <a href="/about-us"><span class='en '>About us</span></a>
+                        <a href="#"><span class='en '>Donations</span></a>
+                        <a href="/support"><span class='en '>Help</span></a>
+                        <a data-controller="ajax-modal" data-url="/popups/feedback" href="#_"><span class='en '>Feedback</span></a>
+                        <a href="/developers"><span class='en '>Developers</span></a>
+                        <a href="/apps"><span class='en '>Download</span></a>
+                        <a href="/privacy"><span class='en '>Privacy</span></a>
+                    </div>
+
+                    <div>
+                        <h5><span class='en '>Useful sites</span></h5>
+                        <a href="#" target="_blank" rel="noopener">Quranicaudio.com</a>
+                        <a href="#" target="_blank" rel="noopener">Salah.com</a>
+                        <a href="#" target="_blank" rel="noopener">Sunnah.com</a>
+                        <a href="#" target="_blank" rel="noopener"> Legacy Quran.com</a>
+                        <a href="#" target="_blank" rel="noopener">Corpus.Quran.com</a>
+                        <a href="#" target="_blank" rel="noopener">QuranReflect.com</a>
+                    </div>
+                    <div>
+                        <h5><span class='en '>Other links</span></h5>
+                        <a href="#" target="_blank" rel="noopener"> We're hiring</a>
+                        <a href="#" target="_blank">Sitemap</a>
+                        <a href="/surah-ya-sin">Surah Yaseen (يس)</a>
+                        <a href="/ayatul-kursi">Ayat Al-Kursi (آية الكرسي)</a>
+                        <a href="/surah-al-kahf">Surah Al-Kahf (الكهف)</a>
+                    </div>
+
+                    <div style="max-width: 345px;">
+                        <p>
+                            <span class='en '>Quran.com is a Sadaqah Jariyah. We hope to make it easy for everyone to read, study, and learn The Noble Quran. The Noble Quran has many names including Al-Quran Al-Kareem, Al-Ketab, Al-Furqan, Al-Maw'itha, Al-Thikr, and Al-Noor.</span>
+                        </p>
+
+                        <p>
+                            &copy; 2021
+                            <a href="#" style="display: inline;">Quran.com</a>. <span class='en '>All Rights Reserved</span>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
