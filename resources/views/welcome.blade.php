854acd9ab40c8a2d9888511e1f64ec6c4e641f25
HasanXfatanE <72065442+HasanXfatanE@users.noreply.github.com>
2021-08-17 23:30:12 +0700

    Add files via upload

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,97 +4,402 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>The Noble Quran</title>
+        <title>Laravel</title>
 
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+        <link rel="stylesheet" href="{{asset('css/app.css')}}">
+<!---->
+    <script src="{{asset('js/app.js')}} defer"></script>
+    <script src="{{asset('js/application-33c3ffb37027f82f32b1.chunk.js')}}"></script>   
+    <script src="{{asset('js/bootstrap.js')}} defer" ></script>
+    <script src="{{asset('js/runtime~application-006109b2f6553db0346e.js')}}"></script>
+    <script src="{{asset('js/vendors~application-ab9132ce4363dc9f8e61.chunk.js')}}"></script>   
+  
 
-            .full-height {
-                height: 100vh;
-            }
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+      
 
-            .position-ref {
-                position: relative;
-            }
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
 
-            .content {
-                text-align: center;
-            }
 
-            .title {
-                font-size: 84px;
-            }
 
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
     </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
+
+    <body class="antialiased">
+        
+
+      
+    <div class="header header--border">
+        <div class="container header-inner">
+            <div class="header-inner__firstpart">
+                <div class="header-inner__firstpart__menu">
+  
+  
+  
+                
+                    <!-- TODO add hamburger to fonts -->
+  
+  
+  
+                   
+  
+  
+                    <img id="site-menu--trigger" class="sidebar-btns" alt="quran.com menu" src="images/icon/hamburger-b328bd84b5f4e6621c239ea54e3511632642ebf56db943aa8b04cce4af6e6f7d.svg" />
                 </div>
-            @endif
+  
+                <a class="logo" href="/index">
+        <strong style="font-weight: bold">Quran</strong>.com</a></div>
 
-            <div class="content">
-                <div class="title m-b-md">
-                    The-nobal-quran
-                </div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+        
+  
+  
+            <div class="header-theme-switch">
+                <div class="theme-language">
+                    <div class="language-wrapper" data-controller="choose-locales">
+                        <select class="language" name="locale" id="language">
+      <option value="en"
+              selected>
+        English
+      </option>
+      <option value="ur"
+              >
+        اردو
+      </option>
+      <option value="ar"
+              >
+        العربية
+      </option>
+      <option value="bn"
+              >
+        বাংলা
+      </option>
+      <option value="fa"
+              >
+        فارسی
+      </option>
+      <option value="fr"
+              >
+        Français
+      </option>
+      <option value="id"
+              >
+        Indonesia
+      </option>
+      <option value="it"
+              >
+        Inglese
+      </option>
+      <option value="nl"
+              >
+        Dutch
+      </option>
+      <option value="pt"
+              >
+        Português
+      </option>
+      <option value="sq"
+              >
+        Shqip
+      </option>
+      <option value="th"
+              >
+        ภาษาไทย
+      </option>
+      <option value="tr"
+              >
+        Türkçe
+      </option>
+  </select>
+                    </div>
+  
+                    <div class="theme-switch" data-controller="theme">
+                        <div class="theme-switch__buttons">
+                            <a class="theme-switch__button dark" data-theme="dark"></a>
+                            <a class="theme-switch__button light" data-theme="light"></a>
+                        </div>
+  
+                        <div class="theme-switch__labels">
+                            <p class="theme-switch__label">Mode</p>
+                        </div>
+                    </div>
+  
                 </div>
             </div>
         </div>
+    </div>
+  
+    <div class="menus hidden" id=menus-wrapper>
+  
+  
+        <div class="menus__right-tab hidden side-menu" data-controller="menu" data-trigger="#site-menu--trigger" data-menu=".site-menu">
+            <div class="site-menu">
+                <div class="site-menu__header">
+                    <p class="text text--black text--large2 text--semibold">Menu</p>
+                    <div id="close-menu" class="icon-x site-menu__icon-x"></div>
+                </div>
+  
+                <div class="theme-language">
+                    <div class="language-wrapper" data-controller="choose-locales">
+                        <select class="language" name="locale" id="language">
+      <option value="en"
+              selected>
+        English
+      </option>
+      <option value="ur"
+              >
+        اردو
+      </option>
+      <option value="ar"
+              >
+        العربية
+      </option>
+      <option value="bn"
+              >
+        বাংলা
+      </option>
+      <option value="fa"
+              >
+        فارسی
+      </option>
+      <option value="fr"
+              >
+        Français
+      </option>
+      <option value="id"
+              >
+        Indonesia
+      </option>
+      <option value="it"
+              >
+        Inglese
+      </option>
+      <option value="nl"
+              >
+        Dutch
+      </option>
+      <option value="pt"
+              >
+        Português
+      </option>
+      <option value="sq"
+              >
+        Shqip
+      </option>
+      <option value="th"
+              >
+        ภาษาไทย
+      </option>
+      <option value="tr"
+              >
+        Türkçe
+      </option>
+  </select>
+                    </div>
+  
+                    <div class="theme-switch" data-controller="theme">
+                        <div class="theme-switch__buttons">
+                            <a class="theme-switch__button dark" data-theme="dark"></a>
+                            <a class="theme-switch__button light" data-theme="light"></a>
+                        </div>
+  
+                        <div class="theme-switch__labels">
+                            <p class="theme-switch__label">Mode</p>
+                        </div>
+                    </div>
+  
+                </div>
+  
+                <div class="site-menu__items">
+                    <div class="menu-item">
+                        <a class="menu-item__link" href="/">
+            <span class="quran-icon icon-home"></span>
+            <p class="text text--grey text--large text--semibold">
+              <span class='en '>Home</span>
+            </p>
+  </a> </div>
+  
+                    <div class="menu-item">
+                        <a class="menu-item__link" href="/apps">
+            <span class="quran-icon icon-mobile"></span>
+            <p class="text text--grey text--large text--semibold">
+              <span class='en '>Mobile Apps</span>
+            </p>
+  </a> </div>
+  
+                    <div class="menu-item">
+                        <a class="menu-item__link" href="/donations">
+            <span class="quran-icon icon-contribute"></span>
+            <p class="text text--grey text--large text--semibold">
+              <span class='en '>Donations</span>
+            </p>
+  </a> </div>
+  
+                    <div class="menu-item">
+                        <a class="menu-item__link" href="/about-us">
+            <span class="quran-icon icon-about"></span>
+            <p class="text text--grey text--large text--semibold">
+              <span class='en '>About us</span>
+            </p>
+  </a> </div>
+  
+                    <div class="menu-item">
+                        <a class="menu-item__link" data-controller="ajax-modal" data-url="/popups/feedback" href="#_">
+            <span class="quran-icon icon-help"></span>
+            <p class="text text--grey text--large text--semibold">
+              Feedback
+            </p>
+  </a> </div>
+  
+                    <div class="menu-item">
+                        <a class="menu-item__link" href="/support">
+            <span class="quran-icon icon-help"></span>
+            <p class="text text--grey text--large text--semibold">
+              <span class='en '>Help</span></p></a></div>
+  
+  
+                    <div class="menu-item">
+                        <a class="menu-item__link" href="/developers">
+            <span class="quran-icon icon-dev"></span>
+            <p class="text text--grey text--large text--semibold">
+              <span class='en '>Developers</span></p></a></div>
+  
+                    <div class="menu-item">
+                        <a class="menu-item__link" href="/privacy">
+            <span class="quran-icon icon-about"></span>
+            <p class="text text--grey text--large text--semibold">
+              <span class='en '>Privacy</span></p></a></div></div>
+  
+                <div class="site-menu__sites">
+                    <p class="text text--large2 text--black text--semibold">
+                        <span class='en '>Related Sites</span>
+                    </p>
+  
+                    <div class="menu-item">
+                        <a class="menu-item__link" href="https://quranicaudio.com/" rel="noopener">
+                            <p class="text text--grey text--large text--semibold">Quranaudio.com</p>
+                        </a>
+                    </div>
+  
+                    <div class="menu-item">
+                        <a class="menu-item__link" href="https://sunnah.com/" rel="noopener">
+                            <p class="text text--grey text--large text--semibold">Sunnah.com</p>
+                        </a>
+                    </div>
+  
+                    <div class="menu-item">
+                        <a class="menu-item__link" href="https://salah.com/" rel="noopener">
+                            <p class="text text--grey text--large text--semibold">Salah.com</p>
+                        </a>
+                    </div>
+  
+                    <div class="menu-item">
+                        <a class="menu-item__link" href="https://legacy.quran.com/" rel="noopener">
+                            <p class="text text--grey text--large text--semibold">Legacy Quran.com</p>
+                        </a>
+                    </div>
+  
+                    <div class="menu-item">
+                        <a class="menu-item__link" href="https://corpus.quran.com/wordbyword.jsp" rel="noopener">
+                            <p class="text text--grey text--large text--semibold">Corpus.Quran.com</p>
+                        </a>
+                    </div>
+  
+                    <div class="menu-item">
+                        <a class="menu-item__link" href="https://quranreflect.com" target="_blank" rel="noopener">
+                            <p class="text text--grey text--large text--semibold">QuranReflect.com</p>
+                        </a>
+                    </div>
+                </div>
+  
+
+
+
+               
+            </div>
+        </div>
+    </div>
+    
+  
+
+
+
+  
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  <!-- main start -->
+  
+  @yield('page-content')
+  
+  <!-- main end -->
+  
+      <!-- Starting of the footer-->
+  
+      <div class="footer">
+        <div class="container">
+            <div class="content-page">
+                <div class="footer-links">
+                    <div>
+                        <h5><span class='en '>Navigate</span></h5>
+                        <a href="/about-us"><span class='en '>About us</span></a>
+                        <a href="#"><span class='en '>Donations</span></a>
+                        <a href="/support"><span class='en '>Help</span></a>
+                        <a data-controller="ajax-modal" data-url="/popups/feedback" href="#_"><span class='en '>Feedback</span></a>
+                        <a href="/developers"><span class='en '>Developers</span></a>
+                        <a href="/apps"><span class='en '>Download</span></a>
+                        <a href="/privacy"><span class='en '>Privacy</span></a>
+                    </div>
+  
+                    <div>
+                        <h5><span class='en '>Useful sites</span></h5>
+                        <a href="#" target="_blank" rel="noopener">Quranicaudio.com</a>
+                        <a href="#" target="_blank" rel="noopener">Salah.com</a>
+                        <a href="#" target="_blank" rel="noopener">Sunnah.com</a>
+                        <a href="#" target="_blank" rel="noopener"> Legacy Quran.com</a>
+                        <a href="#" target="_blank" rel="noopener">Corpus.Quran.com</a>
+                        <a href="#" target="_blank" rel="noopener">QuranReflect.com</a>
+                    </div>
+                    <div>
+                        <h5><span class='en '>Other links</span></h5>
+                        <a href="#" target="_blank" rel="noopener"> We're hiring</a>
+                        <a href="#" target="_blank">Sitemap</a>
+                        <a href="/surah-ya-sin">Surah Yaseen (يس)</a>
+                        <a href="/ayatul-kursi">Ayat Al-Kursi (آية الكرسي)</a>
+                        <a href="/surah-al-kahf">Surah Al-Kahf (الكهف)</a>
+                    </div>
+  
+                    <div style="max-width: 345px;">
+                        <p>
+                            <span class='en '>القرآن الكريم هو صدقة جارية. نأمل أن نسهل على الجميع قراءة القرآن الكريم ودراسته وتعلمه. القرآن الكريم له أسماء عديدة منها القرآن الكريم والكتاب والفرقان والموؤثة والذكر والنور.</span>
+                        </p>
+  
+                        <p>
+                            &copy; 2021
+                            <a href="#" style="display: inline;">Quran.com</a>. <span class='en '>All Rights Reserved</span>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  
+    <!-- Ending of the footer-->
     </body>
 </html>
