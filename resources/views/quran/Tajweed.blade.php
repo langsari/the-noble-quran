e235e2798677b9d284fc2ae89a65a4fc4d57dc65
robbanee77 <robbanaa4959@gmail.com>
2022-10-16 23:40:06 +0700

    Update Tajweed.blade.php

--- resources/views/quran/Tajweed.blade.php
+++ resources/views/quran/Tajweed.blade.php
@@ -12,6 +12,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <style>
+        
         .Tajweed{
             text-align:center;
         }
