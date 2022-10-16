0ce85f355c5f26aea8bcd9c4f61ef42a5e614e75
robbanee77 <robbanaa4959@gmail.com>
2022-10-16 23:32:41 +0700

    Update Tajweed.blade.php

--- resources/views/quran/Tajweed.blade.php
+++ resources/views/quran/Tajweed.blade.php
@@ -1,14 +1,28 @@
-@extends('quran.nav')
-@section('content')
+@extends('quran.navbar')
+<br>
+<br>
+<br>
+<br>
+<br>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Al-Quran</title>
+    <title>Document</title>
+    <style>
+        .Tajweed{
+            text-align:center;
+        }
+       
+    </style>
 </head>
 <body>
-    <H1>Tajweed</H1>
+
+<h1 class="Tajweed"> Tajweed</h1>
+
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio vero fugit explicabo dolorem eaque modi ab ea, minus porro exercitationem dolore, placeat ex accusamus ad tempore, sequi qui maiores consequatur!</p>
+    
 </body>
 </html>
