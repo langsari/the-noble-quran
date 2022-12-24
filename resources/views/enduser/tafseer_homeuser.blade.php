b70962487032d697044e6916660b136c8afeb52f
surai-da <nursuraida.01@gmail.com>
2022-12-24 22:54:47 +0700

    update tafseer home of end user

--- resources/views/enduser/tafseer_homeuser.blade.php
+++ resources/views/enduser/tafseer_homeuser.blade.php
@@ -1,14 +1,53 @@
 @extends('enduser.shortnav')
 @section('content')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel Search with pagination</title>
+    <link rel="stylesheet" href="{{ asset('bootstrap-3.1.1/css/bootstrap.min.css')}}"> 
+</head>
+
+<style>
+  * {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+  .table{
+    font-family: 'Uthmani', serif;
+  }
+  .tabtop{
+    
+
+    background-image: linear-gradient(to right, #abbaab 0%, #ffffff 51%, #abbaab 100%);
+    
+    transition: .5s;
+    
+    
+    box-shadow: 0 0 20px #eee;
+    
+    
+  }
+  .table-hover tbody tr:hover td {
+    background-color: #85FFBD;
+    
+}
+.pagination{
+  display: flex;
+}
+</style>
 <br>
 <br>
 
 
 <div class ="container">
-  <table class="table table-hover table-bordered border-success table-striped">
+  <table class="table table-hover   ">
   
     <thead >
-    <tr class="table-success">
+    <tr class="tabtop">
         <th scope="col">ลำดับที่</th>
         <th scope="col">ชื่อซูเราะห์</th>
         <th scope="col">ชื่อภาษาอาหรับ</th>
@@ -19,20 +58,21 @@
     <tbody>
  
     @foreach($datasurahs as $datasurah)  
-    <tr class="table-success ">
+    <tr >
         <td>{{ $datasurah->id}}</td>
         <td>
        
-          <a href ="{{ route('tafseer_detailuser', $datasurah) }}" class="link-dark" >
+        <a href ="{{ route('tafseer_detailuser', $datasurah) }}" class="link-dark" >
           {{ $datasurah->th_name}}</a></td>
-        <td>{{ $datasurah->surah_arab}})</td>
+        <td>({{ $datasurah->surah_arab}})</td>
         <td>{{ $datasurah->type}}</td>
         <td>{{ $datasurah->whole_ayah}}</td>
     </tr> 
     @endforeach
     </tbody> 
     </table>
+    <div class="pagination justify-content-center">
     {{$datasurahs->links()}}
- 
+    </div>
   </div>
 @endsection
