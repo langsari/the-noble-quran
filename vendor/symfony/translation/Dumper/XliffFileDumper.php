8807b05f3709f5c37e275e3cd4c447cc57b8fe9e
surai-da <nursuraida.01@gmail.com>
2023-01-16 10:53:15 +0700

    Revert "Revert "change your note page""
    This reverts commit 98a6863b0de6f4782821b1b5bb2cf06c7f982486.

--- vendor/symfony/translation/Dumper/XliffFileDumper.php
+++ vendor/symfony/translation/Dumper/XliffFileDumper.php
@@ -21,11 +21,6 @@ use Symfony\Component\Translation\MessageCatalogue;
  */
 class XliffFileDumper extends FileDumper
 {
-    public function __construct(
-        private string $extension = 'xlf',
-    ) {
-    }
-
     /**
      * {@inheritdoc}
      */
@@ -57,7 +52,7 @@ class XliffFileDumper extends FileDumper
      */
     protected function getExtension(): string
     {
-        return $this->extension;
+        return 'xlf';
     }
 
     private function dumpXliff1(string $defaultLocale, MessageCatalogue $messages, ?string $domain, array $options = [])
@@ -84,15 +79,6 @@ class XliffFileDumper extends FileDumper
         $xliffTool = $xliffHead->appendChild($dom->createElement('tool'));
         foreach ($toolInfo as $id => $value) {
             $xliffTool->setAttribute($id, $value);
-        }
-
-        if ($catalogueMetadata = $messages->getCatalogueMetadata('', $domain) ?? []) {
-            $xliffPropGroup = $xliffHead->appendChild($dom->createElement('prop-group'));
-            foreach ($catalogueMetadata as $key => $value) {
-                $xliffProp = $xliffPropGroup->appendChild($dom->createElement('prop'));
-                $xliffProp->setAttribute('prop-type', $key);
-                $xliffProp->appendChild($dom->createTextNode($value));
-            }
         }
 
         $xliffBody = $xliffFile->appendChild($dom->createElement('body'));
@@ -159,16 +145,6 @@ class XliffFileDumper extends FileDumper
             $xliffFile->setAttribute('id', substr($domain, 0, -\strlen(MessageCatalogue::INTL_DOMAIN_SUFFIX)).'.'.$messages->getLocale());
         } else {
             $xliffFile->setAttribute('id', $domain.'.'.$messages->getLocale());
-        }
-
-        if ($catalogueMetadata = $messages->getCatalogueMetadata('', $domain) ?? []) {
-            $xliff->setAttribute('xmlns:m', 'urn:oasis:names:tc:xliff:metadata:2.0');
-            $xliffMetadata = $xliffFile->appendChild($dom->createElement('m:metadata'));
-            foreach ($catalogueMetadata as $key => $value) {
-                $xliffMeta = $xliffMetadata->appendChild($dom->createElement('prop'));
-                $xliffMeta->setAttribute('type', $key);
-                $xliffMeta->appendChild($dom->createTextNode($value));
-            }
         }
 
         foreach ($messages->all($domain) as $source => $target) {
