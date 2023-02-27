416a25324fbee0e31280efb14c55910ba1310917
surai-da <nursuraida.01@gmail.com>
2023-02-27 23:39:10 +0700

    home user dark

--- resources/views/enduser/shortnav.blade.php
+++ resources/views/enduser/shortnav.blade.php
@@ -10,13 +10,14 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Bitter:400,700">
     <link rel="stylesheet" href="{{ URL::to('css/navbar.css') }}">
+    <script  src="{{ URL::to('js/dark-mode.js') }}"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
    <!-- JavaScript Bundle with Popper -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
 
   
 
-<body>
+<body class="bg">
 <div  class="hero ">
 <div  class="topnav" > <!-- BGnav -->
 <a class="active" href="{{ route('homeuser') }}">Quran.th</a>
@@ -47,6 +48,15 @@
        @csrf
     </form>
 </li>
+<div class="dark-light">
+<li class="list active">
+<a href="#" onclick="myFunction()">
+  <span  class="icon"><ion-icon name="moon-outline" ></ion-icon></span>
+  <span class="text">มืด</span>
+</a>
+</li>
+</div>
+
 </ul>
 <script>
   let list = document.querySelectorAll('.list');
